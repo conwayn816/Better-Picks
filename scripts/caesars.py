4c1110ae37d75d1566cfb2898fb9e86e51ebb4e5
conwayn816 <77367414+conwayn816@users.noreply.github.com>
2024-01-23 16:00:38 -0500

    split up work

---
+++ scripts/caesars.py
@@ -0,0 +1,2 @@
+#https://api.americanwagering.com/regions/us/locations/tn/brands/czr/sb/v3/cannedparlays/basketball
+#Kamryn
---
+++ scripts/fanduel.py
@@ -0,0 +1,2 @@
+#Andres
+#https://sbapi.nj.sportsbook.fanduel.com/api/content-managed-page?page=CUSTOM&customPageId=nba&pbHorizontal=false&_ak=FhMFpcPWXMeyZxOx&timezone=America%2FNew_York
