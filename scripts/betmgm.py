55cd94e97216a225ae6b1a190a3356420ff7e12b
conwayn816 <77367414+conwayn816@users.noreply.github.com>
2024-01-23 16:16:25 -0500

    Update betmgm.py

--- scripts/betmgm.py
+++ scripts/betmgm.py
@@ -1,1 +1,2 @@
 # Nate
+'''https://sports.tn.betmgm.com/en/sports/api/widget/widgetdata?layoutSize=Small&page=CompetitionLobby&sportId=7&regionId=9&competitionId=6004&compoundCompetitionId=1:6004&widgetId=/mobilesports-v1.0/layout/layout_us/modules/basketball/nba/nba-gamelines-complobby&shouldIncludePayload=true'''
