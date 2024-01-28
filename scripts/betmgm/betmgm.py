32c377c8aa8b9ac56afa2e6f1b18a348a72a39cb
conwayn816 <77367414+conwayn816@users.noreply.github.com>
2024-01-28 18:30:28 -0500

    set up

--- scripts/betmgm/betmgm.py
+++ scripts/betmgm/betmgm.py
@@ -1,2 +1,21 @@
 # Nate
-'''https://sports.tn.betmgm.com/en/sports/api/widget/widgetdata?layoutSize=Small&page=CompetitionLobby&sportId=7&regionId=9&competitionId=6004&compoundCompetitionId=1:6004&widgetId=/mobilesports-v1.0/layout/layout_us/modules/basketball/nba/nba-gamelines-complobby&shouldIncludePayload=true'''
+import requests
+
+
+
+
+
+if __name__ == "__main__":
+    url = 'https://sports.tn.betmgm.com/en/sports/api/widget/widgetdata?layoutSize=Small& page=CompetitionLobby&sportId=7&regionId=9&competitionId=6004&compoundCompetitionId=1:6004&widgetId=/mobilesports-v1.0/layout/layout_us/modules/basketball/nba/nba-gamelines-complobby&shouldIncludePayload=true'
+
+    response = requests.get(url)   
+
+    if response.status_code == 200:
+        print(response.json())
+    else:
+        print("Error: " + str(response.status_code))
+
+    
+    
+
+
