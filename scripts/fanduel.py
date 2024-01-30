abbc5b8a7566a4816399ad9ac2bdd0e82a0acf74
Ace1000ac <151976524+Ace1000ac@users.noreply.github.com>
2024-01-29 21:17:35 -0500

    added script

--- scripts/fanduel.py
+++ scripts/fanduel.py
@@ -1,2 +1,21 @@
 #Andres
 #https://sbapi.nj.sportsbook.fanduel.com/api/content-managed-page?page=CUSTOM&customPageId=nba&pbHorizontal=false&_ak=FhMFpcPWXMeyZxOx&timezone=America%2FNew_York
+
+import requests
+
+url = "https://sbapi.nj.sportsbook.fanduel.com/api/content-managed-page?page=CUSTOM&customPageId=nba&pbHorizontal=false&_ak=FhMFpcPWXMeyZxOx&timezone=America%2FNew_York"
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    data = response.json()
+  
+    moneylines = []
+
+    if 'moneylines' in data:
+        moneylines = data['moneylines']
+
+    print("Moneylines:", moneylines)
+
+else:
+    print(f"Error Unable to Display Data: {response.status_code}")
