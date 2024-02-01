25ec5f8094eaa84fe6326581c4465875c4964b35
conwayn816 <77367414+conwayn816@users.noreply.github.com>
2024-02-01 13:23:11 -0500

    finished betmgm script

--- scripts/betmgm/betmgm.py
+++ scripts/betmgm/betmgm.py
@@ -2,18 +2,42 @@
 import requests
 import constants
 
+
 def getBets(data, bet_types):
     bets = []
     items = data.get("widgets")[0].get("payload").get("items")
     fixtures = items[0].get("activeChildren")[0].get("payload").get("fixtures")
-    
-    games = [] #stores each game in a list
+
+    events = []  # stores each fixture in a list
     for fixture in fixtures:
-        games.append(fixture.get("games"))
-    
-    
-    
-    
+        events.append(fixture)
+
+    matchups = {}  # stores the event id and the matchup
+    for event in events:
+        id = event.get("id")
+        matchup = event.get("name").get("value")
+        matchups[id] = matchup
+
+    for event in events:
+        for offer in event.get("games"):
+            if offer.get("name").get("value") in bet_types:
+                for result in offer.get("results"):
+                    current_team = result.get("name").get("value")
+                    line = None
+                    if "Over" in current_team or "Under" in current_team:
+                        line = current_team
+                        current_team = "N/A"
+                    else:
+                        current_team = current_team
+                    bets.append(
+                        {
+                            "event": matchups.get(event.get("id")),
+                            "type": offer.get("name").get("value"),
+                            "team": current_team,
+                            "line": line,
+                            "odds": result.get("americanOdds"),
+                        }
+                    )
 
     return bets
 
@@ -21,16 +45,17 @@ def getBets(data, bet_types):
 if __name__ == "__main__":
     url = constants.url
     headers = constants.headers
-    response = requests.get(url, headers=headers)   
+    response = requests.get(url, headers=headers)
 
     if response.status_code == 200:
         data = response.json()
     else:
         print("Error: " + str(response.status_code))
 
-    
-    bet_types = ['Moneyline', 'Spread', 'Total Points'] 
+    bet_types = ["Money Line", "Spread", "Totals"]
 
-    getBets(data, bet_types)
+    extracted_bets = getBets(data, bet_types)
 
-
+    for bet in extracted_bets:
+        print(bet)
+        print("\n")
