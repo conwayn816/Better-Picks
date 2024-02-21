8567d75b7c0ec5268dea168029d251a8748d274d
Ryan Dixon <rmd21c@fsu.edu>
2024-02-21 15:13:32 -0500

    Updated pointbet.py
    Added comments for readability and added a start_time variable to store event start times.

--- scripts/pointsbet.py
+++ scripts/pointsbet.py
@@ -3,19 +3,42 @@ import json
 import re
 import math
 
+# Function to get the bets
 def getBets(data):
+    # Create an empty list to save the bets
     bets = []
+
+    # Loop through the events
     for event in data['events']:
+
+        # Check if the event is an NBA event
         if "NBA" in event['competitionName']:
+
+            # Check if the home team and away team are not empty
             if event['homeTeam'] == '' or event['awayTeam'] == '':
                 continue
+
+            # Create a dictionary to save the bet types
             bet_types = {}
+
+            # Loop through the specialFixedOddsMarkets to find bet types
             for types in event['specialFixedOddsMarkets']:
+
+                # Check if the bet type is Moneyline, Point Spread or Total
                 if types['eventName'] == "Moneyline" or types['eventName'] == "Point Spread" or types['eventName'] == "Total":
+
+                    # Change the bet type name to Spread if it is Point Spread
                     if types['eventName'] == "Point Spread":
                         types['eventName'] = "Spread"
+
+                    # Save the start time of the event
+                    types['startTime'] = types['advertisedStartTime'].replace("T", " ").replace("Z", "")
+
+                    # Save the bet types to the bet_types dictionary
                     bet_type = types['eventName']
                     bet_type_data = []
+
+                    # Loop through the outcomes to get the team, line and odds
                     for outcomes in types['outcomes']:
                         pattern = r'[-+]?\d*\.\d+|\d+'
                         numerical_value = re.findall(pattern, outcomes['name'])
@@ -24,6 +47,7 @@ def getBets(data):
                         else:
                             odds = str(+math.ceil((((outcomes['price'] - 1) * 100) / 5.01)) * 5)
 
+                        # Check if the bet type is Moneyline
                         if types['eventName'] == "Moneyline":
                             team_name = event['homeTeam'] if outcomes['name'].startswith(event['homeTeam']) else event['awayTeam']
                             bet_type_data.append({
@@ -43,9 +67,12 @@ def getBets(data):
                                 'odds': odds,
                             })
                     bet_types[bet_type] = bet_type_data
+                    
+            # Save the bets
             bets.append({
                 'home_team': event['homeTeam'],
                 'away_team': event['awayTeam'],
+                'start_time': types['startTime'],
                 'bets': bet_types
             })
     return bets
