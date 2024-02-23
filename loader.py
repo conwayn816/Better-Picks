037038e63be957ec79ccbb566f0279d191dfc374
conwayn816 <77367414+conwayn816@users.noreply.github.com>
2024-02-23 15:49:03 -0500

    Check if record is already in db

--- loader.py
+++ loader.py
@@ -1,17 +1,18 @@
 from database.models import Bets
 from database.cleanDB import clean_past_bets
-import scripts.betmgm.betmgm as scraper
+import scripts.betmgm.betmgm as mgmScraper
 from mongoengine import connect
 import scripts.betmgm as scraper
 import constants
 import requests
+
 # import json
 from datetime import datetime
 
 
 def load_mgm_bets() -> None:
-    url = constants.url
-    headers = constants.headers
+    url = constants.mgm_url
+    headers = constants.mgm_headers
     response = requests.get(url, headers=headers)
 
     if response.status_code == 200:
@@ -21,62 +22,72 @@ def load_mgm_bets() -> None:
 
     bet_types = ["Money Line", "Spread", "Totals"]
 
-    extracted_bets = scraper.getBets(data, bet_types)
-    organized_bets = scraper.organize_betting_data_ordered(extracted_bets)
+    extracted_bets = mgmScraper.getBets(data, bet_types)
+    organized_bets = mgmScraper.organize_betting_data_ordered(extracted_bets)
 
     # Save to MongoDB
     connect(db="betterPicks", host=constants.MONGO_URI)
     for game in organized_bets:
         datetime_object = datetime.strptime(game["start_time"], "%Y-%m-%dT%H:%M:%SZ")
-        bet = Bets(
+
+        # Check if the game is already in the database
+        existing_game = Bets.objects.filter(
             BetProvider="BetMGM",
             GameTime=datetime_object,
             HomeTeam=game["home_team"],
             AwayTeam=game["away_team"],
-            Bets={
-                "Spread": [
-                    {
-                        "Team": game["bets"]["Spread"][0]["team"],
-                        "Line": game["bets"]["Spread"][0]["line"],
-                        "Odds": game["bets"]["Spread"][0]["odds"],
-                    },
-                    {
-                        "Team": game["bets"]["Spread"][1]["team"],
-                        "Line": game["bets"]["Spread"][1]["line"],
-                        "Odds": game["bets"]["Spread"][1]["odds"],
-                    },
-                ],
-                "Total": [
-                    {
-                        "Team": game["bets"]["Total"][0]["team"],
-                        "Line": game["bets"]["Total"][0]["line"],
-                        "Odds": game["bets"]["Total"][0]["odds"],
-                    },
-                    {
-                        "Team": game["bets"]["Total"][1]["team"],
-                        "Line": game["bets"]["Total"][1]["line"],
-                        "Odds": game["bets"]["Total"][1]["odds"],
-                    },
-                ],
-                "Moneyline": [
-                    {
-                        "Team": game["bets"]["Moneyline"][0]["team"],
-                        "Odds": game["bets"]["Moneyline"][0]["odds"],
-                    },
-                    {
-                        "Team": game["bets"]["Moneyline"][1]["team"],
-                        "Odds": game["bets"]["Moneyline"][1]["odds"],
-                    },
-                ],
-            },
-        )
-        bet.save()
+        ).first()
+
+        # If the game is not in the database, insert it
+        if existing_game is None:
+            bet = Bets(
+                BetProvider="BetMGM",
+                GameTime=datetime_object,
+                HomeTeam=game["home_team"],
+                AwayTeam=game["away_team"],
+                Bets={
+                    "Spread": [
+                        {
+                            "Team": game["bets"]["Spread"][0]["team"],
+                            "Line": game["bets"]["Spread"][0]["line"],
+                            "Odds": game["bets"]["Spread"][0]["odds"],
+                        },
+                        {
+                            "Team": game["bets"]["Spread"][1]["team"],
+                            "Line": game["bets"]["Spread"][1]["line"],
+                            "Odds": game["bets"]["Spread"][1]["odds"],
+                        },
+                    ],
+                    "Total": [
+                        {
+                            "Team": game["bets"]["Total"][0]["team"],
+                            "Line": game["bets"]["Total"][0]["line"],
+                            "Odds": game["bets"]["Total"][0]["odds"],
+                        },
+                        {
+                            "Team": game["bets"]["Total"][1]["team"],
+                            "Line": game["bets"]["Total"][1]["line"],
+                            "Odds": game["bets"]["Total"][1]["odds"],
+                        },
+                    ],
+                    "Moneyline": [
+                        {
+                            "Team": game["bets"]["Moneyline"][0]["team"],
+                            "Odds": game["bets"]["Moneyline"][0]["odds"],
+                        },
+                        {
+                            "Team": game["bets"]["Moneyline"][1]["team"],
+                            "Odds": game["bets"]["Moneyline"][1]["odds"],
+                        },
+                    ],
+                },
+            )
+            bet.save()
 
     # For testing purposes
-    """Print to console
-    for game in Bets.objects:
+    """for game in Bets.objects:
         print(game.HomeTeam, game.AwayTeam)
-        print(game.Bets)
+        print(game.Bets)"""
 
-    prettifiedJson = json.dumps(organized_bets, indent=4)
-    # print(prettifiedJson)"""
+
+load_mgm_bets()
