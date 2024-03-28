4a3cd2ccab39b0a828c69b70e528a8d0e254ba54
Ryan Dixon <rmd21c@fsu.edu>
2024-03-28 17:47:34 -0400

    Web App Update

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template, redirect, url_for
 
+# TEMP DATABASE FOR TESTING
 Bets = [
     {
-        "BetProvider": "Test Provider 1",
+        "BetProvider": "DraftKings",
         "GameTime": "2024-04-01 12:00:00",
         "HomeTeam": "Home Team",
         "AwayTeam": "Away Team",
@@ -12,8 +13,8 @@ Bets = [
                 {"Team": "Away Team", "Line": 3.5, "Odds": -100},
             ],
             "Total": [
-                {"Team": "Over", "Line": 50.5, "Odds": 100},
-                {"Team": "Under", "Line": 50.5, "Odds": -100},
+                {"Team": "Over", "Line": 210.5, "Odds": 100},
+                {"Team": "Under", "Line": 210.5, "Odds": -100},
             ],
             "Moneyline": [
                 {"Team": "Home Team", "Odds": -110},
@@ -22,7 +23,7 @@ Bets = [
         },
     },
     {
-        "BetProvider": "Test Provider 2",
+        "BetProvider": "Caesers Sportsbook",
         "GameTime": "2024-04-01 12:00:00",
         "HomeTeam": "Home Team",
         "AwayTeam": "Away Team",
@@ -32,8 +33,8 @@ Bets = [
                 {"Team": "Away Team", "Line": 4.5, "Odds": -100},
             ],
             "Total": [
-                {"Team": "Over", "Line": 50.5, "Odds": 200},
-                {"Team": "Under", "Line": 50.5, "Odds": -200},
+                {"Team": "Over", "Line": 224.5, "Odds": 200},
+                {"Team": "Under", "Line": 224.5, "Odds": -200},
             ],
             "Moneyline": [
                 {"Team": "Home Team", "Odds": -100},
@@ -42,7 +43,7 @@ Bets = [
         },
     },
     {
-        "BetProvider": "Test Provide 4",
+        "BetProvider": "PointsBet",
         "GameTime": "2024-04-01 12:00:00",
         "HomeTeam": "Home Team",
         "AwayTeam": "Away Team",
@@ -52,8 +53,8 @@ Bets = [
                 {"Team": "Away Team", "Line": 3.5, "Odds": -100},
             ],
             "Total": [
-                {"Team": "Over", "Line": 50.5, "Odds": 100},
-                {"Team": "Under", "Line": 50.5, "Odds": -100},
+                {"Team": "Over", "Line": 212.5, "Odds": 100},
+                {"Team": "Under", "Line": 212.5, "Odds": -100},
             ],
             "Moneyline": [
                 {"Team": "Home Team", "Odds": -110},
@@ -62,7 +63,7 @@ Bets = [
         },
     },
     {
-        "BetProvider": "Test Provider 3",
+        "BetProvider": "BetMGM",
         "GameTime": "2024-04-01 12:00:00",
         "HomeTeam": "Home Team",
         "AwayTeam": "Away Team",
@@ -72,8 +73,8 @@ Bets = [
                 {"Team": "Away Team", "Line": 4.5, "Odds": -100},
             ],
             "Total": [
-                {"Team": "Over", "Line": 50.5, "Odds": 200},
-                {"Team": "Under", "Line": 50.5, "Odds": -200},
+                {"Team": "Over", "Line": 200.5, "Odds": 200},
+                {"Team": "Under", "Line": 200.5, "Odds": -200},
             ],
             "Moneyline": [
                 {"Team": "Home Team", "Odds": -100},
@@ -85,25 +86,54 @@ Bets = [
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
+    return redirect(url_for('moneyline'))
+
+@app.route('/moneyline', methods=['GET', 'POST'])
+def moneyline():
     active_view = 'Moneyline'
-    moneyline_bets = [] 
+    game_bets = {}
     for bet in Bets:
-        moneyline_bets.append({
+        game_key = (bet['HomeTeam'], bet['AwayTeam'])
+        if game_key not in game_bets:
+            game_bets[game_key] = {'HomeTeam': bet['HomeTeam'], 'AwayTeam': bet['AwayTeam'], 'bets': []}
+        game_bets[game_key]['bets'].append({
             'BetProvider': bet['BetProvider'],
             'HomeTeamBet': bet['Bets']['Moneyline'][0],
             'AwayTeamBet': bet['Bets']['Moneyline'][1]
         })
-        
-    return render_template('index.html', title='Moneyline', box_items=moneyline_bets, active_view=active_view)
+    return render_template('moneyline.html', box_items=game_bets.values(), active_view=active_view)
 
-# NONFUNCTIONAL SWITCH VIEW
-@app.route('/<view_name>', methods=['GET', 'POST'])
-def switch_view(view_name):
-    if view_name not in ['Spread', 'Total', 'Moneyline']:
-        return redirect(url_for('index'))
-    return render_template('index.html', **{'title': view_name, 'box_items': Bets[0]['Bets'][view_name]}, active_view=view_name)
+@app.route('/spread', methods=['GET', 'POST'])
+def spread():
+    active_view = 'Spread'
+    game_bets = {}
+    for bet in Bets:
+        game_key = (bet['HomeTeam'], bet['AwayTeam'])
+        if game_key not in game_bets:
+            game_bets[game_key] = {'HomeTeam': bet['HomeTeam'], 'AwayTeam': bet['AwayTeam'], 'bets': []}
+        game_bets[game_key]['bets'].append({
+            'BetProvider': bet['BetProvider'],
+            'HomeTeamBet': bet['Bets']['Spread'][0],
+            'AwayTeamBet': bet['Bets']['Spread'][1]
+        })
+    return render_template('spread.html', box_items=game_bets.values(), active_view=active_view)
+
+@app.route('/total', methods=['GET', 'POST'])
+def total():
+    active_view = 'Total'
+    game_bets = {}
+    for bet in Bets:
+        game_key = (bet['HomeTeam'], bet['AwayTeam'])
+        if game_key not in game_bets:
+            game_bets[game_key] = {'HomeTeam': bet['HomeTeam'], 'AwayTeam': bet['AwayTeam'], 'bets': []}
+        game_bets[game_key]['bets'].append({
+            'BetProvider': bet['BetProvider'],
+            'HomeTeamBet': bet['Bets']['Total'][0],
+            'AwayTeamBet': bet['Bets']['Total'][1]
+        })
+    return render_template('total.html', box_items=game_bets.values(), active_view=active_view)
 
 if __name__ == '__main__':
     app.run(debug=True)
