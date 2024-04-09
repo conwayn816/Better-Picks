68b5b12f40aaa87abb70563c3c37078826177611
Ace1000ac <151976524+Ace1000ac@users.noreply.github.com>
2024-04-09 15:55:10 -0400

    added date code

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from pymongo import MongoClient
 import constants
 from teamMap import TEAM_MAP
 from loader import load_bets
+from datetime import datetime, timedelta
 
 # TEMP DATABASE FOR TESTING
 '''
@@ -113,8 +114,11 @@ def refresh():
 @app.route('/moneyline', methods=['GET', 'POST'])
 def moneyline():
     active_view = 'Moneyline'
+    current_date = datetime.now().strftime('%Y-%m-%d')
+    start_of_day = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
+    end_of_day = start_of_day + timedelta(days=1)
     game_bets = {}
-    for doc in db.bets.find():
+    for doc in db.bets.find({'GameTime': {'$gte': start_of_day, '$lt': end_of_day}}):
         if doc['HomeTeam'] in TEAM_MAP:
             hometeam = TEAM_MAP[doc['HomeTeam']]
         else:
@@ -126,19 +130,24 @@ def moneyline():
         game_key = (hometeam, awayteam)
         if game_key not in game_bets:
             game_bets[game_key] = {'HomeTeam': hometeam, 'AwayTeam': awayteam, 'bets': []}
+        game_time = doc['GameTime'].strftime('%I:%M %p CST')
+        game_bets[game_key]['GameTime'] = game_time
         game_bets[game_key]['bets'].append({
             'BetProvider': doc['BetProvider'],
             'HomeTeamBet': doc['Bets']['Moneyline'][0],
             'AwayTeamBet': doc['Bets']['Moneyline'][1]
         })
     
-    return render_template('moneyline.html', box_items=game_bets.values(), active_view=active_view)
+    return render_template('moneyline.html', box_items=game_bets.values(), active_view=active_view, current_date=start_of_day)
 
 @app.route('/spread', methods=['GET', 'POST'])
 def spread():
     active_view = 'Spread'
+    current_date = datetime.now().strftime('%Y-%m-%d')
+    start_of_day = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
+    end_of_day = start_of_day + timedelta(days=1)
     game_bets = {}
-    for doc in db.bets.find():
+    for doc in db.bets.find({'GameTime': {'$gte': start_of_day, '$lt': end_of_day}}):
         if doc['HomeTeam'] in TEAM_MAP:
             hometeam = TEAM_MAP[doc['HomeTeam']]
         else:
@@ -180,13 +189,16 @@ def spread():
             'AwayTeamBet': bet['Bets']['Spread'][1]
         })
     '''
-    return render_template('spread.html', box_items=game_bets.values(), active_view=active_view)
+    return render_template('spread.html', box_items=game_bets.values(), active_view=active_view, current_date=start_of_day)
 
 @app.route('/total', methods=['GET', 'POST'])
 def total():
     active_view = 'Total'
+    current_date = datetime.now().strftime('%Y-%m-%d')
+    start_of_day = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
+    end_of_day = start_of_day + timedelta(days=1)
     game_bets = {}
-    for doc in db.bets.find():
+    for doc in db.bets.find({'GameTime': {'$gte': start_of_day, '$lt': end_of_day}}):
         if doc['HomeTeam'] in TEAM_MAP:
             hometeam = TEAM_MAP[doc['HomeTeam']]
         else:
@@ -214,7 +226,7 @@ def total():
             'AwayTeamBet': bet['Bets']['Total'][1]
         })
     '''
-    return render_template('total.html', box_items=game_bets.values(), active_view=active_view)
+    return render_template('total.html', box_items=game_bets.values(), active_view=active_view, current_date=start_of_day)
 '''
 need to add uname settings function, pword settings function, friends function, and account history
 function once we have another mongo db collection setup that includes users/pwords, friends 
